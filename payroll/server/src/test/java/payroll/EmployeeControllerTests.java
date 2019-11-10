a1225f2c1556958932a4ff431a975df48fd25ce3
Octavio <1610251@ucc.edu.ar>
2019-11-10 16:14:13 -0300

    Test agregados

--- payroll/server/src/test/java/payroll/EmployeeControllerTests.java
+++ payroll/server/src/test/java/payroll/EmployeeControllerTests.java
@@ -46,13 +46,13 @@ public class EmployeeControllerTests extends AbstractTest {
 		.andExpect(jsonPath("$._embedded.employeeList[0].firstName", is("Bilbo")))
 		.andExpect(jsonPath("$._embedded.employeeList[0].lastName", is("Baggins")))
 		.andExpect(jsonPath("$._embedded.employeeList[0].role", is("ring bearer")))
-		//.andExpect(jsonPath("$._embedded.employeeList[0]._links.self.href", is("http://localhost/employees/1")))
+		.andExpect(jsonPath("$._embedded.employeeList[0]._links.self.href", is("http://localhost/employees/1")))
 		.andExpect(jsonPath("$._embedded.employeeList[0]._links.employees.href", is("http://localhost/employees")))
 		.andExpect(jsonPath("$._embedded.employeeList[1].id", is(2)))
 		.andExpect(jsonPath("$._embedded.employeeList[1].firstName", is("Fodo")))
 		.andExpect(jsonPath("$._embedded.employeeList[1].lastName", is("Baggins")))
 		.andExpect(jsonPath("$._embedded.employeeList[1].role", is("burglar")))
-		//.andExpect(jsonPath("$._embedded.employeeList[1]._links.self.href", is("http://localhost/employees/2")))
+		.andExpect(jsonPath("$._embedded.employeeList[1]._links.self.href", is("http://localhost/employees/2")))
 		.andExpect(jsonPath("$._embedded.employeeList[1]._links.employees.href", is("http://localhost/employees")))
 		.andExpect(jsonPath("$._links.self.href", is("http://localhost/employees"))) //
 		.andReturn();
@@ -61,25 +61,25 @@ public class EmployeeControllerTests extends AbstractTest {
 	@Test
 	public void getEmployees_Test_3() throws Exception {
 
-//		given(repository.findAll()).willReturn( //
-//		Arrays.asList( //
-//				new Employee("Frodo1", "Baggins", "ring bearer"), //
-//				new Employee("Bilbo1", "Baggins", "burglar")));
+		given(repository.findAll()).willReturn( //
+		Arrays.asList( //
+				new Employee("Frodo1", "Baggins", "ring bearer"), //
+				new Employee("Bilbo1", "Baggins", "burglar")));
 		
 		mvc.perform(get("/employees").accept(MediaTypes.HAL_JSON_VALUE))
 		.andDo(print())
 		.andExpect(status().isOk())
-		//.andExpect(jsonPath("$._embedded.employeeList[0].id", is(1)))
+		.andExpect(jsonPath("$._embedded.employeeList[0].id", is(1)))
 		.andExpect(jsonPath("$._embedded.employeeList[0].firstName", is("Frodo1")))
 		.andExpect(jsonPath("$._embedded.employeeList[0].lastName", is("Baggins")))
 		.andExpect(jsonPath("$._embedded.employeeList[0].role", is("ring bearer")))
-		//.andExpect(jsonPath("$._embedded.employeeList[0]._links.self.href", is("http://localhost/employees/1")))
+		.andExpect(jsonPath("$._embedded.employeeList[0]._links.self.href", is("http://localhost/employees/1")))
 		.andExpect(jsonPath("$._embedded.employeeList[0]._links.employees.href", is("http://localhost/employees")))
-		//.andExpect(jsonPath("$._embedded.employeeList[1].id", is(2)))
+		.andExpect(jsonPath("$._embedded.employeeList[1].id", is(2)))
 		.andExpect(jsonPath("$._embedded.employeeList[1].firstName", is("Bilbo1")))
 		.andExpect(jsonPath("$._embedded.employeeList[1].lastName", is("Baggins")))
 		.andExpect(jsonPath("$._embedded.employeeList[1].role", is("burglar")))
-		//.andExpect(jsonPath("$._embedded.employeeList[1]._links.self.href", is("http://localhost/employees/2")))
+		.andExpect(jsonPath("$._embedded.employeeList[1]._links.self.href", is("http://localhost/employees/2")))
 		.andExpect(jsonPath("$._embedded.employeeList[1]._links.employees.href", is("http://localhost/employees")))
 		.andExpect(jsonPath("$._links.self.href", is("http://localhost/employees"))) //
 		.andReturn();
